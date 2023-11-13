fe06127d329e8e5243491efeef89dbd0f489ae9f
Benardlusisa <adybenard20@gmail.com>
2023-11-13 11:50:12 +0300

    chnages made

--- php/config.php
+++ php/config.php
@@ -1,14 +1,29 @@
 <?php
-class databaseConnection{
-	public function __construct(){
-		global $conn;
-		$conn = new mysqli("localhost", "root", "" , "uni");
-		//check error 
-		if(!$conn){
-			die("Database cannot established connection properly: " . $conn->connect_error());
-		}
 
-	}
+class DatabaseConnection {
+    public function __construct() {
+        global $conn;
+
+        // Check if running on Heroku (using the CLEARDB_DATABASE_URL environment variable)
+        if (getenv("CLEARDB_DATABASE_URL")) {
+            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+            $server = $url["host"];
+            $username = $url["user"];
+            $password = $url["pass"];
+            $db = substr($url["path"], 1);
+
+            $conn = new mysqli($server, $username, $password, $db);
+        } else {
+            // Use local database configuration
+            $conn = new mysqli("localhost", "root", "", "uni");
+        }
+
+        // Check for errors
+        if ($conn->connect_error) {
+            die("Database connection failed: " . $conn->connect_error);
+        }
+    }
 }
 
 ?>
